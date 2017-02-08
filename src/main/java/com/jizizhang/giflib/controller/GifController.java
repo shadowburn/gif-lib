b0c2bc037eea652400eefac7a74d60b1a4b41002
shadowburn <zjzottawa@gmail.com>
2017-02-08 00:35:35 -0500

    GifController

---
+++ src/main/java/com/jizizhang/giflib/controller/GifController.java
@@ -0,0 +1,13 @@
+package com.jizizhang.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping(value = "/")
+    public String listGifs(){
+        return "home";
+    }
+}
