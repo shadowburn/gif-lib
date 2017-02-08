cd0795aa62c88f71b986e3f89493acd14f3a983e
shadowburn <zjzottawa@gmail.com>
2017-02-07 21:44:14 -0500

    Initial commit

---
+++ src/main/java/com/jizizhang/giflib/AppConfig.java
@@ -0,0 +1,12 @@
+package com.jizizhang.giflib;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class AppConfig {
+    public static void main(String[] args){
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
