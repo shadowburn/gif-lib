bcb4f908b4fa2dd3d58e85edfb176921bd1225ed
shadowburn <zjzottawa@gmail.com>
2017-02-09 02:05:55 -0500

    GifController Content Added

--- src/main/java/com/jizizhang/giflib/controller/GifController.java
+++ src/main/java/com/jizizhang/giflib/controller/GifController.java
@@ -1,13 +1,24 @@
 package com.jizizhang.giflib.controller;
 
+import com.jizizhang.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
     @RequestMapping(value = "/")
     public String listGifs(){
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("android-explosion", LocalDate.of(2017,2,9), "Jizi Zhang", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
